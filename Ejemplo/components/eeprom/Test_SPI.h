b7765c0e534f55c5279bad081a8545fdb9ac4031
SantiBarrios2002 <santiago.pablo.barrios@estudiantat.upc.edu>
2025-04-24 11:11:42 +0200

    Example code removed from files

--- Ejemplo/components/eeprom/Test_SPI.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEST_SPI_H
-#define TEST_SPI_H
-
-void initialize_ad5940(void);
-void validate_ad5940_id(void);
-void validate_ad5940_write(); // Llamada a la prueba de escritura
-
-#endif // TEST_SPI_H
