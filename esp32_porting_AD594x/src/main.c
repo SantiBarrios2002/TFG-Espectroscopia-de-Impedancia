15338a577fd0b521aff289864283a83fbed361a9
SantiBarrios2002 <santiago.pablo.barrios@estudiantat.upc.edu>
2025-06-06 13:29:39 +0200

    basic main.c created

--- esp32_porting_AD594x/src/main.c
+++ esp32_porting_AD594x/src/main.c
@@ -1,18 +1,39 @@
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_log.h"
+/*
 
-static const char *TAG = "MAIN";
+Copyright (c) 2017-2019 Analog Devices, Inc. All Rights Reserved.
 
-void app_main(void)
+This software is proprietary to Analog Devices, Inc. and its licensors.
+By using this software you agree to the terms of the associated
+Analog Devices Software License Agreement.
+
+*/
+
+#include "stdio.h"
+#include "ad5940.h"
+
+/* Functions that used to initialize MCU platform */
+uint32_t MCUPlatformInit(void *pCfg);
+
+int main(void)
 {
-    ESP_LOGI(TAG, "Starting ESP32 application");
-    
-    // Your code here
-    
-    while (1) {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
+  void AD5940_Main(void);
+  MCUPlatformInit(0);
+  AD5940_MCUResourceInit(0);
+  printf("Hello AD5940-Build Time:%s\n",__TIME__);
+  AD5940_Main();
+}
+
+/* Below functions are used to initialize MCU Platform */
+uint32_t MCUPlatformInit(void *pCfg)
+{
+  /* Clock Configure */
+  /* Configure system clock */
+  
+  /* UART Configure */
+  /* Configure UART for debug output */
+  
+  /* GPIO Configure */
+  /* Configure GPIO pins */
+  
+  return 0;
 }
